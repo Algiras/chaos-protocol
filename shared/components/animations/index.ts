81fd770b90adc230e1b699f6d755c7364564208e
Aligmantas Krasauskas <algimantask@wix.com>
2026-02-10 19:56:31 +0200

    fix: export new animation components in index

--- shared/components/animations/index.ts
+++ shared/components/animations/index.ts
@@ -2,3 +2,7 @@ export { FadeIn } from './FadeIn';
 export { CountUp } from './CountUp';
 export { StaggerContainer, StaggerItem } from './StaggerContainer';
 export { PageTransition } from './PageTransition';
+export { ParallaxSection } from './ParallaxSection';
+export { ScaleOnScroll } from './ScaleOnScroll';
+export { RevealOnScroll } from './RevealOnScroll';
+export { GlowOnHover } from './GlowOnHover';
